726c398ae5a2cf917ea28e3b625eb4bdac87e1d8
Àlex Barba <alexander.barba@gmail.com>
2019-02-12 16:28:27 +0100

    v1

---
+++ staff/alex-barba/tcp-chat/server.js
@@ -0,0 +1,10 @@
+const net = require('net')
+
+const { argv:[,, port] } = process
+
+net.createServer(conn => 
+    conn.on('data', data => {
+        console.log(data.toString())
+        conn.end('Roger that!')
+    })
+).listen(port)
